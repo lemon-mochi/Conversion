50f2325bae5ea8eca815bd198a8d84fd2b68b80f
lm_thebrain <genwblaine@gmail.com>
2024-01-03 14:49:09 -0800

    add volume and area

--- conversion.java
+++ conversion.java
@@ -21,7 +21,7 @@ public class conversion {
         return fathom * 1.852;
     }
 
-    private static double f_to_b(double b) {
+    private static double b_to_f(double b) {
         double sqrt_5_13 = Math.sqrt(5.0 / 13.0);
         if (b < 65) {
             return 50.0 - 10.0 * sqrt_5_13 * Math.sqrt(65.0 - b);
@@ -31,8 +31,48 @@ public class conversion {
         }
     }
 
-    private static double b_to_f(double f) {
+    private static double f_to_b(double f) {
         return (7.0 / 500) * Math.abs(f - 50) * (f - 50) + 65;
+    }
+
+    private static double perch_to_sqm(double perch) {
+        return 25.29285264 * perch;
+    }
+
+    private static double sq_mile_to_sqkm(double sq_mile) {
+        return 2.58999 * sq_mile;
+    }
+
+    private static double sq_ft_to_sqm(double sq_ft) {
+        return 0.09290304 * sq_ft;
+    }
+
+    private static double sq_i_to_sq_cm(double sq_i) {
+        return 6.4516 * sq_i;
+    }
+
+    private static double imp_q_to_l(double imp_q) {
+        return 1.1365225 * imp_q;
+    }
+
+    private static double us_lq_to_l(double us_lq) {
+        return 0.946352946 * us_lq;
+    }
+
+    private static double us_dq_to_l(double us_dq) {
+        return 1.101220942715 * us_dq;
+    }
+
+    private static double imp_c_to_l(double imp_c) {
+        return 0.28413064262468  * imp_c;
+    }
+
+    private static double us_c_to_l(double us_c) {
+        return 0.2365882365  * us_c;
+    }
+
+    private static double us_teas_to_ml(double us_teas) {
+        return 4.9289215938 * us_teas;
     }
 
     private static void updateComboBox(JComboBox<String> topbox, JComboBox<String> comboBox) {
@@ -51,6 +91,17 @@ public class conversion {
                 break;
             case "temperature":
                 units = new String[]{"Kelvin", "Fahrenheit", "Blaine"};
+                break;
+            case "area":
+                units = new String[]{"square inch", "square foot", "perch", "rood", "acre", 
+                        "square mile"};
+                break;
+            case "volume":
+                units = new String[]{"US teaspoon", "Imperial teaspoon", "US tablespoon", 
+                        "Imperial tablespoon", "Imperial fluid ounce", "US fluid ounce", 
+                        "US liquid pint", "US cup", "Imperial cup", "US dry pint", "Imperial pint", 
+                        "US liquid quart", "US dry quart", "Imperial quart", "US liquid gallon",
+                        "US dry gallon", "Imperial gallon"};
                 break;
             default:
                 units = new String[]{};
@@ -267,12 +318,137 @@ public class conversion {
                         }
 
                         else if ("Blaine".equals(selectedValue)) {
-                            double of = b_to_f(u_value);
+                            double of = f_to_b(u_value);
                             oc = (of - 32) * 5 / 9;
                         }
                         else oc = 0.0;
 
                         resultLabel.setText("Result: " + oc + " degrees Celsius");
+                    }
+
+                    else if ("area".equals(topValue[0])) {
+                        double sqm;
+                        if ("perch".equals(selectedValue)) {
+                            sqm = perch_to_sqm(u_value);
+                            resultLabel.setText("Result: " + sqm + " m^2");
+                        }
+
+                        else if ("rood".equals(selectedValue)) {
+                            sqm = perch_to_sqm(u_value / 40);
+                            resultLabel.setText("Result: " + sqm + " m^2");
+                        }
+
+                        else if ("acre".equals(selectedValue)) {
+                            sqm = perch_to_sqm(u_value / 160);
+                            resultLabel.setText("Result: " + sqm + " m^2");
+                        }
+
+                        else if ("square inch".equals(selectedValue)) {
+                            double sqcm = sq_i_to_sq_cm(u_value);
+                            resultLabel.setText("Result: " + sqcm + " cm^2");
+                        }
+
+                        else if ("square foot".equals(selectedValue)) {
+                            sqm = sq_ft_to_sqm(u_value);
+                            resultLabel.setText("Result: " + sqm + " m^2");
+                        }
+
+                        else if ("square mile".equals(selectedValue)) {
+                            double sqkm = sq_mile_to_sqkm(u_value);
+                            resultLabel.setText("Result: " + sqkm + " km^2");
+                        }
+                    }
+
+                    else if ("volume".equals(topValue[0])) {
+                        double l;
+                        if ("Imperial fluid ounce".equals(selectedValue)) {
+                            l = imp_q_to_l(u_value / 40);
+                            double mL = l * 1000;
+                            resultLabel.setText("Result: " + mL + " mL");
+                        }
+
+                        else if ("Imperial pint".equals(selectedValue)) {
+                            l = imp_q_to_l(u_value / 2);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("Imperial quart".equals(selectedValue)) {
+                            l = imp_q_to_l(u_value);
+                            resultLabel.setText("Result: " + l + " L");                           
+                        }
+
+                        else if ("Imperial gallon".equals(selectedValue)) {
+                            l = imp_q_to_l(u_value * 4);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("US fluid ounce".equals(selectedValue)) {
+                            l = us_lq_to_l(u_value / 32);
+                            double mL = l * 1000;
+                            resultLabel.setText("Result: " + mL + " mL"); 
+                        }
+
+                        else if ("US lquid pint".equals(selectedValue)) {
+                            l = us_lq_to_l(u_value / 2);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("US dry pint".equals(selectedValue)) {
+                            l = us_dq_to_l(u_value / 2);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("US liquid quart".equals(selectedValue)) {
+                            l = us_lq_to_l(u_value);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("US dry quart".equals(selectedValue)) {
+                            l = us_dq_to_l(u_value);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("US liquid gallon".equals(selectedValue)) {
+                            l = us_lq_to_l(u_value * 4);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("US dry gallon".equals(selectedValue)) {
+                            l = us_dq_to_l(u_value * 4);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("Imperial teaspoon".equals(selectedValue)) {
+                            l = imp_c_to_l(u_value / 48);
+                            double mL = l * 1000;
+                            resultLabel.setText("Result: " + mL + " mL");
+                        }
+
+                        else if ("Imperial tablespoon".equals(selectedValue)) {
+                            l = imp_c_to_l(u_value / 16);
+                            double mL = l * 1000;
+                            resultLabel.setText("Result: " + mL + " mL");
+                        }
+
+                        else if ("Imperial cup".equals(selectedValue)) {
+                            l = imp_c_to_l(u_value);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
+
+                        else if ("US teaspoon".equals(selectedValue)) {
+                            double mL = us_teas_to_ml(u_value);
+                            resultLabel.setText("Result: " + mL + " mL");
+                        }
+
+                        else if ("US tablespoon".equals(selectedValue)) {
+                            double mL = us_teas_to_ml(u_value * 3);
+                            resultLabel.setText("Result: " + mL + " mL");
+                        }
+
+                        else if ("US cup".equals(selectedValue)) {
+                            l = us_c_to_l(u_value);
+                            resultLabel.setText("Result: " + l + " L");
+                        }
                     }
                     
                 } catch (NumberFormatException ex) {
@@ -283,8 +459,8 @@ public class conversion {
         });
 
         JPanel panel = new JPanel();
-        panel.add(topbox);
-        panel.add(comboBox);
+        panel.add(topbox); //topbox is the thing that lists the unit types like temp, length
+        panel.add(comboBox); //comboBox is the thing that lists the measurement units
         panel.add(new JLabel("Enter Value: "));
         panel.add(textField);
         panel.add(submitButton);
